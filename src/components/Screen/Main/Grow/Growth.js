01a3e3f0898c9cb19cb37ad1fdee407502833aee
ParkYoSep <op5321@soongsil.ac.kr>
2020-11-03 09:29:41 +0900

    성장페이지 제작 시작

--- src/components/Screen/Main/Grow/Growth.js
+++ src/components/Screen/Main/Grow/Growth.js
@@ -1,10 +1,76 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+    View, 
+    Text, 
+    StyleSheet,
+    Image,
+    Button,
+    TouchableOpacity,
+    ScrollView
+} from 'react-native';
 
 export default Growth = () => {
     return(
-        <View>
-            <Text>Growth</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.top}>
+                <View style={styles.topLeft}>
+                    <Image style={styles.topImg} source={require('../../../../assets/img/night.png')}/>
+                </View>
+                <View style={styles.topRight}>
+                    {/* 경험치바 */}
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            title='모do 개설'
+                        />
+                    </View>
+                </View>
+            </View>
+            <View style={styles.bottom}>
+                <Image style={styles.listImg} source={require('../../../../assets/img/night.png')}/>
+                <View style={styles.listTextContainer}>
+                    <Text style={styles.listTitle}>매일 물 마시기</Text>
+                    <TouchableOpacity>
+                        <Text style={styles.listButton}>인증하기</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container:{
+
+    },
+    top:{
+
+    },
+    topLeft:{
+
+    },
+    topImg:{
+
+    },
+    topRight:{
+
+    },
+    buttonContainer:{
+
+    },
+    bottom:{
+
+    },
+    listImg:{
+
+    },
+    listTextContainer:{
+
+    },
+    listTitle:{
+
+    },
+    listButton:{
+
+    },
+})
