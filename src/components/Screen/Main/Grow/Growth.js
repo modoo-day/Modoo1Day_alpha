c2908c73f0ea681d38445b4acf1444bdc5ab96ad
ParkYoSep <op5321@soongsil.ac.kr>
2020-11-03 10:23:32 +0900

    성장페이지 제작

--- src/components/Screen/Main/Grow/Growth.js
+++ src/components/Screen/Main/Grow/Growth.js
@@ -17,7 +17,7 @@ export default Growth = () => {
                     <Image style={styles.topImg} source={require('../../../../assets/img/night.png')}/>
                 </View>
                 <View style={styles.topRight}>
-                    {/* 경험치바 */}
+                    <Text>경험치바</Text>
                     <View style={styles.buttonContainer}>
                         <Button
                             title='모do 개설'
@@ -26,12 +26,33 @@ export default Growth = () => {
                 </View>
             </View>
             <View style={styles.bottom}>
-                <Image style={styles.listImg} source={require('../../../../assets/img/night.png')}/>
-                <View style={styles.listTextContainer}>
-                    <Text style={styles.listTitle}>매일 물 마시기</Text>
-                    <TouchableOpacity>
-                        <Text style={styles.listButton}>인증하기</Text>
-                    </TouchableOpacity>
+
+                <View style={styles.listContainer}>
+                    <Image style={styles.listImg} source={require('../../../../assets/img/night.png')}/>
+                    <View style={styles.listTextContainer}>
+                        <Text style={styles.listTitle}>매일 물 마시기</Text>
+                        <TouchableOpacity>
+                            <Text style={styles.listButton}>인증하기</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.listContainer}>
+                    <Image style={styles.listImg} source={require('../../../../assets/img/night.png')}/>
+                    <View style={styles.listTextContainer}>
+                        <Text style={styles.listTitle}>매일 물 마시기</Text>
+                        <TouchableOpacity>
+                            <Text style={styles.listButton}>인증하기</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.listContainer}>
+                    <Image style={styles.listImg} source={require('../../../../assets/img/night.png')}/>
+                    <View style={styles.listTextContainer}>
+                        <Text style={styles.listTitle}>매일 물 마시기</Text>
+                        <TouchableOpacity>
+                            <Text style={styles.listButton}>인증하기</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         </ScrollView>
@@ -41,36 +62,62 @@ export default Growth = () => {
 
 const styles = StyleSheet.create({
     container:{
-
+        //backgroundColor:'lightblue',
+        marginHorizontal:'5%'
     },
     top:{
-
+        //backgroundColor:'lightblue',
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginVertical:'3%'
     },
     topLeft:{
-
+        //backgroundColor:'pink',
     },
     topImg:{
-
+        height:100,
+        width:100,
+        borderRadius:10,
+        borderWidth:3
     },
     topRight:{
-
+        //backgroundColor:'lightyellow',
+        width:'55%',
+        justifyContent:'space-between',
+        paddingVertical:'5%'
     },
     buttonContainer:{
-
+        width:'100%'
     },
     bottom:{
-
+        //backgroundColor:'lightblue',
+        borderBottomWidth:1.5,
+        paddingBottom:5
+    },
+    listContainer:{
+        flex:1,
+        backgroundColor:'lightgrey',
+        flexDirection:'row',
+        alignItems:'center',
+        borderRadius:10,
+        marginVertical:'2.5%'
     },
     listImg:{
-
+        height:60,
+        width:60,
+        borderRadius:10
     },
     listTextContainer:{
-
+        top:6,
+        width:'70%',
+        //backgroundColor:'lightblue',
+        marginLeft:'5%'
     },
     listTitle:{
 
     },
     listButton:{
-
+        alignSelf:'flex-end'
     },
 })
