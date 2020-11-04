bb0e845b5005f2c7acc1ccfd4059b016975ad728
ParkYoSep <op5321@soongsil.ac.kr>
2020-11-04 14:08:27 +0900

    피드페이지 이미지 삽입 틀 제작중

--- src/components/Screen/Main/Feed/Feed.js
+++ src/components/Screen/Main/Feed/Feed.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+    View, 
+    Text, 
+    StyleSheet,
+    Image,
+    ScrollView,
+    TouchableOpacity
+} from 'react-native';
 
 export default Feed = () => {
     return(
-        <View>
-            <Text>Feed</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+            {/* <Image style={styles.feedImg} source={require('../../../../assets/img/night.png')} /> */}
+            <TouchableOpacity style={styles.imgContainer}>
+                <Image style={styles.feedImg} source={require('../../../../assets/img/night.png')}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.imgContainer}>
+                <Image style={styles.feedImg} source={require('../../../../assets/img/night.png')}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.imgContainer}>
+                <Image style={styles.feedImg} source={require('../../../../assets/img/night.png')}/>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.imgContainer}>
+                <Image style={styles.feedImg} source={require('../../../../assets/img/night.png')}/>
+            </TouchableOpacity>
+            
+        </ScrollView>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        flexWrap:'wrap',
+        marginHorizontal:'1%',
+        backgroundColor:'grey'
+        },
+    imgContainer:{
+        backgroundColor:'lightyellow',
+        width:125,
+        height:125,
+        marginTop:'1%'
+    },
+    feedImg:{
+        height:'100%',
+        width:'100%'
+    }
+})
