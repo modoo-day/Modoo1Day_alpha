4d741ef465399957f4c31f453f82ebbc512aeaf1
ParkYoSep <op5321@soongsil.ac.kr>
2020-11-05 10:31:36 +0900

    search 페이지 검색바 구현 (react-native-elements)사용

--- src/components/Screen/Modal/Search/Search.js
+++ src/components/Screen/Modal/Search/Search.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
-export default Search = () => {
+
+const Search = () => {
+
+    const [search, setSearch] = useState('');
+
     return(
         <View>
             <Text>Search</Text>
+            <SearchBar
+                placeholder="Type Here..."
+                value={search}
+                onChangeText={(text) => setSearch(text)}
+            />
         </View>
     )
 }
+
+export default Search;
