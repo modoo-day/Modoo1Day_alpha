ffd99c09db77f5a01e1d7ae196bb0dce51a86f50
ParkYoSep <op5321@soongsil.ac.kr>
2020-11-12 08:38:02 +0900

    OpenC 세부기능 구현

--- src/components/Screen/Main/Grow/Opened/OpenC.js
+++ src/components/Screen/Main/Grow/Opened/OpenC.js
@@ -12,33 +12,41 @@ import Button from 'apsl-react-native-button';
 const OpenB = ({navigation}) =>{
 
     const [btn, setBtn] = useState('');
+    const [participants, setParticipants] = useState('');
+
     let button1;
     let button2;
     let button3;
     
     if (btn == '1일모두') {
         button1 =  <Button
-                        style={styles.button}
+                        style={participants=='100~5000' ?styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
+                        onPress={()=> participants!='100~5000' ? setParticipants('100~5000') : setParticipants('')}
+                        //onPress={()=>setParticipants('100~5000')}
                     >
                         100명~5000명
                     </Button>
     } else if (btn == '5일모두'||btn == '7일모두') {
         button1 =  <Button
-                        style={styles.button}
+                        style={participants=='2~100' ?styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
+                        onPress={()=> participants!='2~100' ? setParticipants('2~100') : setParticipants('')}
+
                     >
                         2명~100명
                     </Button> ;
         button2 = <Button
-                        style={styles.button}
+                        style={participants=='101~500' ?styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
+                        onPress={()=> participants!='101~500' ? setParticipants('101~500') : setParticipants('')}
                     >
                         101명~500명
                     </Button>;
         button3 = <Button
-                        style={styles.button}
+                        style={participants=='501~5000' ?styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
+                        onPress={()=> participants!='501~5000' ? setParticipants('501~5000') : setParticipants('')}
                     >
                         501명~5000명
                     </Button>;
@@ -54,7 +62,7 @@ const OpenB = ({navigation}) =>{
                     <Button
                         style={btn=='1일모두' ?styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
-                        onPress={()=>setBtn('1일모두')}
+                        onPress={()=>{setBtn('1일모두'), setParticipants('')}}
                     >
                         1일 모두
                     </Button>
@@ -63,7 +71,7 @@ const OpenB = ({navigation}) =>{
                     <Button
                         style={btn=='5일모두' ?styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
-                        onPress={()=>setBtn('5일모두')}
+                        onPress={()=>{setBtn('5일모두'), setParticipants('')}}
                     >
                         5일(평일) 모두
                     </Button>
@@ -72,7 +80,7 @@ const OpenB = ({navigation}) =>{
                     <Button
                         style={btn=='7일모두' ?styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
-                        onPress={()=>setBtn('7일모두')}
+                        onPress={()=>{setBtn('7일모두'), setParticipants('')}}
                     >
                         7일(평일*주말) 모두
                     </Button>
@@ -104,8 +112,8 @@ const OpenB = ({navigation}) =>{
                 <View style={styles.buttonContainer}>
 
                     <Button
-                        style={styles.deepButton}
-                        textStyle={styles.deepButtonText}
+                        style={styles.deepButton1}
+                        textStyle={styles.deepButtonText1}
                         // //activeOpacity={1}
                         // disabledStyle={{backgroundColor: 'white'}}
                         // isDisabled={email == '' || password == '' ? true : false}
@@ -113,11 +121,11 @@ const OpenB = ({navigation}) =>{
                         onPress={() => navigation.goBack() }
                     >취소</Button>
                     <Button
-                        style={styles.deepButton}
-                        textStyle={styles.deepButtonText}
+                        style={styles.deepButton2}
+                        textStyle={styles.deepButtonText2}
                         // //activeOpacity={1}
-                        // disabledStyle={{backgroundColor: 'white'}}
-                        // isDisabled={email == '' || password == '' ? true : false}
+                        disabledStyle={{backgroundColor: 'white'}}
+                        isDisabled={participants==false ? true : false}
                         // isLoading={false}
                         onPress={() => navigation.navigate('OpenD') }
                     >계속</Button>
@@ -233,7 +241,7 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         height:50,
     },
-    deepButton:{
+    deepButton1:{
         width: '50%',
         backgroundColor: 'white', 
         //'#ffcd2c',
@@ -242,7 +250,20 @@ const styles = StyleSheet.create({
         borderRadius: 0,
         alignSelf: 'center',
     },
-    deepButtonText:{
+    deepButtonText1:{
+        fontFamily: 'neodgm',
+        fontSize: 10,
+    },
+    deepButton2:{
+        width: '50%',
+        backgroundColor: '#ffcd2c', 
+        //'#ffcd2c',
+        borderWidth: 2,
+        height: '100%',
+        borderRadius: 0,
+        alignSelf: 'center',
+    },
+    deepButtonText2:{
         fontFamily: 'neodgm',
         fontSize: 10,
     },
