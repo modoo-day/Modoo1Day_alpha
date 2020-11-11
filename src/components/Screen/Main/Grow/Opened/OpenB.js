314d21d8db8e390537030bae5944751136bf775a
ParkYoSep <op5321@soongsil.ac.kr>
2020-11-12 08:22:17 +0900

    OpenB 세부기능 구현

--- src/components/Screen/Main/Grow/Opened/OpenB.js
+++ src/components/Screen/Main/Grow/Opened/OpenB.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     View,
     Text,
@@ -10,6 +10,22 @@ import Button from 'apsl-react-native-button';
 
 
 const OpenB = ({navigation}) =>{
+
+    const [btn1, setBtn1] = useState(false); // 누구나
+    const [btn2, setBtn2] = useState(false); // 비밀번호를 공유한 사람만
+    const [password, setPassword] = useState('');
+
+    let passwordInput;
+
+    if (btn2 == true) {
+        passwordInput = <TextInput
+                            style={styles.passwordInput}
+                            placeholder='숫자 4자리'
+                            value={password}
+                            onChangeText={(contents) => setPassword(contents)}
+                        />
+    }
+
     return(
         <ScrollView>
             <View style={styles.container}>
@@ -18,31 +34,34 @@ const OpenB = ({navigation}) =>{
                 </View>
                 <View style={styles.mid}>
                     <Button
-                        style={styles.button}
+                        style={btn1==true? styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
+                        disabledStyle={{backgroundColor: 'white'}}
+                        onPress={()=> btn1==false ? setBtn1(true) : setBtn1(false)}
+                        //isDisabled={btn1==false ? true : false}
                     >
                         누구나
                     </Button>
                 </View>
                 <View style={styles.bottom}>
                     <Button
-                        style={styles.button}
+                        style={btn2==true? styles.activeButton : styles.button}
                         textStyle={styles.buttonText}
+                        disabledStyle={{backgroundColor: 'white'}}
+                        onPress={()=> btn2==false ? setBtn2(true) : setBtn2(false)}
+                        //isDisabled={btn1==false ? true : false}
                     >
                         비민번호를 공유한 사람만
                     </Button>
-                    <TextInput
-                        style={styles.passwordInput}
-                        placeholder='숫자 4자리'
-                    />
+                    {passwordInput}
                 </View>
             </View>
             <View style={styles.deep}>
                 <View style={styles.buttonContainer}>
 
                     <Button
-                        style={styles.deepButton}
-                        textStyle={styles.deepButtonText}
+                        style={styles.deepButton1}
+                        textStyle={styles.deepButtonText1}
                         // //activeOpacity={1}
                         // disabledStyle={{backgroundColor: 'white'}}
                         // isDisabled={email == '' || password == '' ? true : false}
@@ -50,11 +69,11 @@ const OpenB = ({navigation}) =>{
                         onPress={() => navigation.goBack() }
                     >취소</Button>
                     <Button
-                        style={styles.deepButton}
-                        textStyle={styles.deepButtonText}
+                        style={styles.deepButton2}
+                        textStyle={styles.deepButtonText2}
                         // //activeOpacity={1}
-                        // disabledStyle={{backgroundColor: 'white'}}
-                        // isDisabled={email == '' || password == '' ? true : false}
+                        disabledStyle={{backgroundColor: 'white'}}
+                        isDisabled={btn1==false && password == '' ? true : false}
                         // isLoading={false}
                         onPress={() => navigation.navigate('OpenC') }
                     >계속</Button>
@@ -84,7 +103,11 @@ const styles = StyleSheet.create({
         backgroundColor:'pink',
         height:50
     },
-    
+    activeButton:{
+        backgroundColor:'#ffcd2c',
+        width:'55%',
+        height:'80%'
+    },
     button:{
         backgroundColor:'white',
         width:'55%',
@@ -116,7 +139,7 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         height:50,
     },
-    deepButton:{
+    deepButton1:{
         width: '50%',
         backgroundColor: 'white', 
         //'#ffcd2c',
@@ -125,7 +148,20 @@ const styles = StyleSheet.create({
         borderRadius: 0,
         alignSelf: 'center',
     },
-    deepButtonText:{
+    deepButtonText1:{
+        fontFamily: 'neodgm',
+        fontSize: 10,
+    },
+    deepButton2:{
+        width: '50%',
+        backgroundColor: '#ffcd2c', 
+        //'#ffcd2c',
+        borderWidth: 2,
+        height: '100%',
+        borderRadius: 0,
+        alignSelf: 'center',
+    },
+    deepButtonText2:{
         fontFamily: 'neodgm',
         fontSize: 10,
     },
