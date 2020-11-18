c7b3bf39268a8fdaf9a206ac62e48cb27f9086d5
op5321 <op5321@soongsil.ac.kr>
2020-11-18 15:40:48 +0900

    AttendanceParticipants 출석인원 레이아웃 제작

--- src/components/Screen/Main/Market/StatusChild/Attendance/AttendanceParticipants.js
+++ src/components/Screen/Main/Market/StatusChild/Attendance/AttendanceParticipants.js
@@ -13,15 +13,32 @@ const AttendanceParticipants = () => {
     return(
         <ScrollView contentContainerStyle={styles.container}>
             <View style={styles.top}>
-                <Text>DAY 1</Text>
-                <Text>참여자 10명</Text>
+                <View style={{flex:1}}></View>
+                <Text style={styles.day}>DAY 1</Text>
+                <Text style={styles.participants}>참여자 10명</Text>
             </View>
             <View style={styles.attendanceContainer}>
-                <Text style={styles.attendanceText}>출석 8/10</Text>
+                <View style={styles.attendanceTextContainer}>
+                    <Text style={styles.attendanceText}>출석 8/10</Text>
+                </View>
                 <View style={styles.profileListContainer}>
                     <View style={styles.profileContainer}>
                         <Image style={styles.profileImg} source={require('../../../../../../assets/img/night.png')}/>
-                        <Text style={styles.id}>아이디</Text>
+                        <View style={styles.idContainer}>
+                            <Text style={styles.id}>아이디</Text>
+                        </View>
+                    </View>
+                    <View style={styles.profileContainer}>
+                        <Image style={styles.profileImg} source={require('../../../../../../assets/img/night.png')}/>
+                        <View style={styles.idContainer}>
+                            <Text style={styles.id}>아이디</Text>
+                        </View>
+                    </View>
+                    <View style={styles.profileContainer}>
+                        <Image style={styles.profileImg} source={require('../../../../../../assets/img/night.png')}/>
+                        <View style={styles.idContainer}>
+                            <Text style={styles.id}>아이디</Text>
+                        </View>
                     </View>
                 </View>
             </View>
@@ -38,23 +55,55 @@ const styles = StyleSheet.create({
     container:{
 
     },
-    top:{
-
+    top:{  
+        backgroundColor:'pink',
+        flexDirection:'row',
+        paddingVertical:30,
+        justifyContent:'space-between'
+    },
+    day:{
+        flex:1,
+        textAlign:'center'
+    },
+    participants:{
+        flex:1,
+        textAlign:'center'
     },
     attendanceContainer:{
-
+        backgroundColor:'lightblue'
+    },
+    attendanceTextContainer:{
+        height:100,
+        justifyContent:'center'
     },
     attendanceText:{
 
     },
     profileListContainer:{
-
+        backgroundColor:'lightgreen',
+        flexDirection:'row',
+        flexWrap:'wrap',
+        borderRadius:10,
+        borderWidth:1
     },
     profileContainer:{
-
+        backgroundColor:'lightyellow',
+        flexDirection:'row',
+        width:'50%',
+        justifyContent:'space-between',
+        paddingHorizontal:'3%',
+        alignItems:'center',
+        borderRadius:30
     },
     profileImg:{
-
+        height:60,
+        width:60,
+        borderRadius:50,
+    },
+    idContainer:{
+        backgroundColor:'lightgrey',
+        width:'50%',
+        alignItems:'center'
     },
     id:{
 
