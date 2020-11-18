e56e5429d8db8dd754d9b014292df431973c5984
op5321 <op5321@soongsil.ac.kr>
2020-11-18 14:53:05 +0900

    출석체크 페이지 구현 중

--- src/components/Screen/Main/Market/StatusChild/Attendance/Attendance.js
+++ src/components/Screen/Main/Market/StatusChild/Attendance/Attendance.js
@@ -3,19 +3,89 @@ import {
     View,
     Text,
     StyleSheet,
-
+    ScrollView,
+    Image,
     } from 'react-native';
     
 const Attendance = () => {
     return(
-        <View>
-            <Text>출석체크fasdfsadfs</Text>
-        </View>
+        <ScrollView contentContainerStyle={styles.container}>
+            <View style={styles.top}>
+                <Text style={styles.progressText}>전체 달성률 85%</Text>
+                <View style={styles.progressContainer}>
+
+                </View>
+            </View>
+            <View style={styles.mid}>
+                <View style={styles.oneCheck}>
+                    <View style={styles.besideCircle}></View>
+                    <View>
+
+                    </View>
+                    <View style={styles.circle}>
+
+                    </View>
+                    <View style={styles.besideCircle}>
+                        <Text style={styles.exp}>
+                            EXP
+                        </Text>
+                        <Text style={styles.coin}>
+                            COIN
+                        </Text>
+                    </View>
+                </View>       
+            </View>
+        </ScrollView>
     )
 }
 
 export default Attendance;
 
 const styles = StyleSheet.create({
-    
+    top:{
+        backgroundColor:'pink',
+        height:100,
+        width:'100%',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    progressText:{
+
+    },
+    progressContainer:{
+        height:30,
+        width:'80%',
+        backgroundColor:'grey',
+        borderRadius:50,
+        marginTop:'5%'
+    },
+    mid:{
+        backgroundColor:'lightblue',
+        
+    },
+    oneCheck:{
+        backgroundColor:'lightgreen',
+        flexDirection:'row',
+        //justifyContent:'center',
+        paddingVertical:20
+    },
+    circle:{
+        height:45,
+        width:45,
+        backgroundColor:'#fdd835',
+        borderRadius:50,
+        borderWidth:3,
+        alignSelf:'center'
+    },
+    besideCircle:{
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    exp:{
+        marginLeft:'10%'
+    },
+    coin:{
+        marginLeft:'10%'
+    }
 })
