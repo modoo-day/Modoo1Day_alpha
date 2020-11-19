dc0ce07e3c90b46611ad67e24046a1a391eab6be
op5321 <op5321@soongsil.ac.kr>
2020-11-19 16:16:55 +0900

    알림 토글 기능 오류 수정

--- src/components/Screen/Modal/Account/AccountChild/NotificationSet.js
+++ src/components/Screen/Modal/Account/AccountChild/NotificationSet.js
@@ -12,8 +12,17 @@ import {
 
 const NotificationSet = ({navigation}) => {
 
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const [isEnabled1, setIsEnabled1] = useState(false);
+    const [isEnabled2, setIsEnabled2] = useState(false);
+    const [isEnabled3, setIsEnabled3] = useState(false);
+    const [isEnabled4, setIsEnabled4] = useState(false);
+    const [isEnabled5, setIsEnabled5] = useState(false);
+
+    const toggleSwitch1 = () => setIsEnabled1(previousState => !previousState);
+    const toggleSwitch2 = () => setIsEnabled2(previousState => !previousState);
+    const toggleSwitch3 = () => setIsEnabled3(previousState => !previousState);
+    const toggleSwitch4 = () => setIsEnabled4(previousState => !previousState);
+    const toggleSwitch5 = () => setIsEnabled5(previousState => !previousState);
 
     return(
         <ScrollView contentContainerStyle={styles.container}>
@@ -24,50 +33,50 @@ const NotificationSet = ({navigation}) => {
                 <Text style = {styles.text}>모두 일시 중단</Text>
                 <Switch
                     trackColor={{ false: "#767577", true: "#81b0ff" }}
-                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    thumbColor={isEnabled1 ? "#f5dd4b" : "#f4f3f4"}
                     ios_backgroundColor="#3e3e3e"
-                    onValueChange={toggleSwitch}
-                    value={isEnabled}
+                    onValueChange={toggleSwitch1}
+                    value={isEnabled1}
                 />
             </View>
             <View style={styles.list}>
                 <Text style = {styles.text}>콘텐츠 알림</Text>
                 <Switch
                     trackColor={{ false: "#767577", true: "#81b0ff" }}
-                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    thumbColor={isEnabled2 ? "#f5dd4b" : "#f4f3f4"}
                     ios_backgroundColor="#3e3e3e"
-                    onValueChange={toggleSwitch}
-                    value={isEnabled}
+                    onValueChange={toggleSwitch2}
+                    value={isEnabled2}
                 />
             </View>
             <View style={styles.list}>
                 <Text style = {styles.text}>콘텐츠 추천</Text>
                 <Switch
                     trackColor={{ false: "#767577", true: "#81b0ff" }}
-                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    thumbColor={isEnabled3 ? "#f5dd4b" : "#f4f3f4"}
                     ios_backgroundColor="#3e3e3e"
-                    onValueChange={toggleSwitch}
-                    value={isEnabled}
+                    onValueChange={toggleSwitch3}
+                    value={isEnabled3}
                 />
             </View>
             <View style={styles.list}>
                 <Text style = {styles.text}>인증샷 및 댓글</Text>
                 <Switch
                     trackColor={{ false: "#767577", true: "#81b0ff" }}
-                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    thumbColor={isEnabled4 ? "#f5dd4b" : "#f4f3f4"}
                     ios_backgroundColor="#3e3e3e"
-                    onValueChange={toggleSwitch}
-                    value={isEnabled}
+                    onValueChange={toggleSwitch4}
+                    value={isEnabled4}
                 />
             </View>
             <View style={styles.list}>
                 <Text style = {styles.text}>팔로잉 및 팔로워</Text>
                 <Switch
                     trackColor={{ false: "#767577", true: "#81b0ff" }}
-                    thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                    thumbColor={isEnabled5 ? "#f5dd4b" : "#f4f3f4"}
                     ios_backgroundColor="#3e3e3e"
-                    onValueChange={toggleSwitch}
-                    value={isEnabled}
+                    onValueChange={toggleSwitch5}
+                    value={isEnabled5}
                 />
             </View>
           
